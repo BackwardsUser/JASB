3146f7bd5963bb71e2aa38645c6102b2a9fa5dfa
Backwards <AlexHardingYT@gmail.com>
2023-06-28 12:48:46 -0400

    Created Support Request Default Embed

---
+++ src/Embeds/SupportRequestEmbed.ts
@@ -0,0 +1,15 @@
+import { Embed, EmbedBuilder, Message } from "discord.js";
+import ExtendedClient from "../Client";
+
+export function SupportRequestEmbed(client: ExtendedClient, message: Message): EmbedBuilder {
+    const embed = client.config.EMBEDS.filter(embed => embed.NAME === "SupportRequestEmbed")[0];
+    const Color = client.config.SERVER_COLOR[embed.DATA.COLOR_POS];
+    return new EmbedBuilder()
+    .setTitle(embed.DATA.TITLE)
+    .setDescription(embed.DATA.DESCRIPTION)
+    .setColor(Color)
+    .setFooter({
+        iconURL: client.user.avatarURL(),
+        text: client.user.username
+    })
+}

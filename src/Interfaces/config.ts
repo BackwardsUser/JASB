f975252715d7d1c31400a84e05923fc3a2fd282b
Backwards <AlexHardingYT@gmail.com>
2023-07-04 18:49:23 -0400

    Added Prefix to Config. Moved Typing to Types Folder

--- src/Interfaces/config.ts
+++ src/Interfaces/config.ts
@@ -1,46 +1,48 @@
 import { ColorResolvable } from "discord.js";
 
 interface Support {
-    SUPPORT_TICKET_CLOSED: boolean,
-    SUPPORT_TICKET_CREATED: boolean,
-    SUPPORT_TICKET_CLOSED_THREAD: boolean,
+    SUPPORT_TICKET_CLOSED: boolean
+    SUPPORT_TICKET_CREATED: boolean
+    SUPPORT_TICKET_CLOSED_THREAD: boolean
     ALERT_STAFF: boolean
 }
 
 interface UpdatesChannel {
-    CHANNEL: string | null,
-    USER_JOIN: boolean,
-    USER_LEAVE: boolean,
-    USERS_USERNAME_UPDATES: boolean,
-    USERS_PROFILE_PICTURE_UPDATES: boolean,
+    CHANNEL: string | null
+    USER_JOIN: boolean
+    USER_LEAVE: boolean
+    USERS_USERNAME_UPDATES: boolean
+    USERS_PROFILE_PICTURE_UPDATES: boolean
     SUPPORT: Support
 }
 
 interface Staff {
-    ROLE: boolean,
-    ROLES?: string[],
+    ROLE: boolean
+    ROLES?: string[]
     USERS: string[]
 }
 
 interface EmbedData {
-    TITLE: string,
-    COLOR_POS: number,
-    DESCRIPTION: string,
+    TITLE: string
+    COLOR_POS: number
+    DESCRIPTION: string
+    ACTION_ROW: boolean
 }
 
 interface Embed {
-    NAME: string,
+    NAME: string
     DATA: EmbedData
 }
 
 export interface Config {
-    TOKEN: string,
-    STAFF: Staff,
-    BLACKLIST: string[],
-    SERVER_NAME: string,
-    SERVER_COLOR: ColorResolvable[],
-    IDENTIFY_STAFF: boolean,
-    UPDATES_CHANNEL: UpdatesChannel,
+    TOKEN: string
+    PREFIX: string
+    STAFF: Staff
+    BLACKLIST: string[]
+    SERVER_NAME: string
+    SERVER_COLOR: ColorResolvable[]
+    IDENTIFY_STAFF: boolean
+    UPDATES_CHANNEL: UpdatesChannel
     EMBEDS: Embed[]
 }
 
